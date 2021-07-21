13a7f7cec938bf07277ecc466de234527921ff1b
Srikanth Govindaraghavan <sri.gr81@gmail.com>
2021-07-21 21:13:04 +0530

    add tests to check deployment and name of contract

--- test/Color.test.js
+++ test/Color.test.js
@@ -1,13 +1,33 @@
+const { assert } = require('chai')
+
 const Color = artifacts.require('./Color.sol')
 
 require('chai').use(require('chai-as-promised')).should()
 
 contract('Color', (accounts) => {
 
+    let contract
+
+    before(async() => {
+
+        contract = await Color.deployed()
+    })
+
     describe("Deployment", async() => {
 
         it("Somehow manages to deploy...", async() => {
 
+            const address = contract.address
+            assert.notEqual(address, undefined)
+            assert.notEqual(address, 0x0)
+            assert.notEqual(address, '')
+            assert.notEqual(address, null)
+        })
+        
+        it("Actually has a name!", async() => {
+
+            const name = await contract.name()
+            assert.equal(name, "Color")
 
         })
 
