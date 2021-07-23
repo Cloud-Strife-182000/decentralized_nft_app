b42d03b98fe729e64c6857a52ab53ab2a8021efc
Srikanth Govindaraghavan <sri.gr81@gmail.com>
2021-07-23 20:04:23 +0530

    add mint function to contract

--- src/contracts/Color.sol
+++ src/contracts/Color.sol
@@ -4,9 +4,18 @@ import "./ERC721Full.sol";
 
 contract Color is ERC721Full{
 
+    string[] public colors;
+    mapping(string => bool) existingColors;
+
     constructor() ERC721Full("Color", "COLOR") public {
 
+    }
 
+    function mint(string memory _color) public {
+
+       uint index = colors.push(_color);
+       _mint(msg.sender, index);
+       existingColors[_color] = true;
     }
 
 }
