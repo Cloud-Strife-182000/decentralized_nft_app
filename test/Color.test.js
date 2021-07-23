daaba764c9511ade26bac76bc570ff6486cbec30
Srikanth Govindaraghavan <sri.gr81@gmail.com>
2021-07-23 20:23:48 +0530

    add tests for minting

--- test/Color.test.js
+++ test/Color.test.js
@@ -32,4 +32,19 @@ contract('Color', (accounts) => {
         })
 
     })
+
+    describe("Minting stuff...", async() => {
+
+        it("Creates a new token!", async() => {
+
+            const result = await contract.mint("#EC058E")
+            const totalSupply = await contract.totalSupply()
+            assert.equal(totalSupply, 1)
+            const event = result.logs[0].args
+            assert.equal(event.tokenId.toNumber(), 1, 'Correct ID!')
+            assert.equal(event.from, '0x0000000000000000000000000000000000000000', 'From address is accurate.')
+            assert.equal(event.to, accounts[0], 'To address is accurate')
+
+        })
+    })
 })
